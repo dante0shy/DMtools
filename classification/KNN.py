840ae002277e7f89d088deadd8367b72a9e9a385
dante0shy <dante0shy@yahoo.com>
2016-08-30 16:07:00 +0800

    some changes

--- classification/KNN.py
+++ classification/KNN.py
@@ -3,6 +3,7 @@ __version__ = '0.1'
 __date__ = '25/08/2016'
 
 from dataW import data
+import random
 from data_dis import data_dis
 from data_pro import data_pro
 import numpy as np
@@ -13,6 +14,13 @@ import log
 import math
 from operator import itemgetter
 
+
+#def KNN(data,data_set,goal_col ,K=2)
+# calculate the KNN alllgroithmn
+#   data    :   the data need to be processed
+#   data_set:   the train data_set,must be all propecessed
+#   goal_col:   the column which need to be classification
+#   K       :   the K in KNN
 def KNN(data,data_set,goal_col ,K=2):
     i=0
     data_NN=[]
@@ -20,9 +28,29 @@ def KNN(data,data_set,goal_col ,K=2):
     data.ix[ :,goal_col]= 0
     #print(data)
     data_all=pd.concat([data_set, data], axis=0,keys=['a','b'])
+    j=0
+    while(j<len(data_set.columns)):
+        if(goal_col==data_set.columns[j]):
+            break
+        j=j+1
+    if(data_set.columns[j]==data_set.columns[0]):
+        col_set=[(data_set.columns[1],data_set.columns[-1])]
+    elif(data_set.columns[j]==data_set.columns[1]):
+        if(not pd.isnull(data_set.columns[2])and data_set.columns[2]!=data_set.columns[-1]):
+            col_set=[data_set.columns[0],(data_set.columns[2],data_set.columns[-1])]
+        elif(not pd.isnull(data_set.columns[2])and data_set.columns[2]==data_set.columns[-1]):
+            col_set = [data_set.columns[0],data_set.columns[-1]]
+        else:
+            col_set = [data_set.columns[0]]
+    elif(data_set.columns[j]==data_set.columns[-2]):
+        col_set=[(data_set.columns[0],data_set.columns[-3]),data_set.columns[-1]]
+    elif(data_set.columns[j]==data_set.columns[-1]):
+        col_set = [(data_set.columns[0], data_set.columns[-2])]
+    else:
+        col_set = [(data_set.columns[0], data_set.columns[j-1]),(data_set.columns[j+1], data_set.columns[-1])]
     #print(data_all.ix[10,:])
     while(i<len(data_all.ix['a'])):
-        dist=data_dis.Minkowski(data_all,index=[i,len(data_all.ix['a'])],col=[])
+        dist=data_dis.Minkowski(data_all,index=[i,len(data_all.ix['a'])],col=col_set)
         #print(dist)
         data_NN.append(tuple((i,dist)))
         data_NN=sorted(data_NN,key =itemgetter(1))
@@ -33,14 +61,22 @@ def KNN(data,data_set,goal_col ,K=2):
         if(len(data_NN)>K):
             del data_NN[len(data_NN)-1]
         i=i+1
+    return data_NN
+    #result=[]
+    #for NN in data_NN:
+     #   result.append([NN[1],data_set[NN[0]].ix[:,goal_col]])
+
+
+
 
 
 
 df = pd.DataFrame(np.random.rand(10,4),columns=['a','b','c','d'])
-df.ix[0,'d']=1
+for i in range(10):
+    df.ix[i,'d']=random.randint(0,1)
+
 
 df1=pd.DataFrame({'a':[1],'b':[2],'c':[3]})
-
 
 
 
--- data_dis/data_dis.py
+++ data_dis/data_dis.py
@@ -1,6 +1,6 @@
 
 __author__ = 'dante0shy'
-__version__ = '0.1'
+__version__ = '0.2'
 __date__ = '24/08/2016'
 
 
@@ -24,28 +24,37 @@ import math
 #   index   :   two data need to compare
 #   col     :   the columns which particpate in the calculate
 #               the columns need to be in pair
-#               Exampl: col = ['v1','v5'] means column v1-v5
-#                       col = ['v1','v1'] means column v1
-#                       col = ['v1','v3','v5','v7'] means column v1-v3 ,v5-v7
+#               Exampl: col = [('v1','v5')] means column v1-v5
+#                       col = ['v1'] means column v1
+#                       col = [('v1','v3'),('v5','v7')] means column v1-v3 ,v5-v7
 def Minkowski(data , r =2,index=[0,0],col =[('@start','@end')]):
         dist = 0.0
         i=0
         j=0
-        if ((col[0]== '@start')):
-            col=[data.columns[0],data.columns[len(data.columns)-1]]
+        if ((col[0]== tuple(('@start',"@end")))):
+            col=[(data.columns[0],data.columns[len(data.columns)-1])]
         else:
             while(j<len(data.columns)):
-                if(col[0] == data.columns[j] ):
-                    break
+                if(isinstance(col[0],tuple)):
+                    if(col[0][0] == data.columns[j] ):
+                        break
+                else:
+                    if (col[0] == data.columns[j]):
+                        break
                 j=j+1
+
 
         while(i<len(col)):
             while (j < len(data.columns)):
-                if (col[i] == data.columns[j]):
-                    break
+                if (isinstance(col[i],tuple)):
+                    if (col[i][0] == data.columns[j]):
+                        break
+                else:
+                    if (col[i] == data.columns[j]):
+                        break
                 j = j + 1
-            if(i+1<len(col)):
-                while(col[i+1] != data.columns[j]):
+            if(isinstance(col[i],tuple)):
+                while(col[i][1] != data.columns[j]):
                     if(pd.isnull(data.ix[index[0],j]) or pd.isnull(data.ix[index[1],j])  ):
                         log.LogMessage("null number!")
                         sys.exit(0)
@@ -56,24 +65,24 @@ def Minkowski(data , r =2,index=[0,0],col =[('@start','@end')]):
                     sys.exit(0)
                 dist = dist + math.pow(abs(float(data.ix[index[0], j]) - float(data.ix[index[1], j])),r)
                 j=j+1
-                i=i+2
-
             else:
                 if (pd.isnull(data.ix[index[0], j]) or pd.isnull(data.ix[index[1], j])):
                     log.LogMessage("null number!")
                     sys.exit(0)
                 dist =dist + math.pow(abs(float(data.ix[index[0],j])-float(data.ix[index[1],j])),r)
-                i = i+1
+            i = i+1
         dist = math.pow(dist,(1/r))
         return dist
 
+
+#test data
 
 
 #data_1 = data.data('../input/BNPcsv.zip','train.csv')
 #data_1.data=data_1.data.fillna(data_1.data.mean())
 #print(data_1.data.ix[ :,'v1'])
 
-#print(distance.Minkowski(data = data_1.data,index=[0,1],col = ['v1','v2']))
+#print(Minkowski(data = data_1.data,index=[0,1],col = [('v1','v2'),'v4']))
 
 #print(Minkowski(data_1  = data_1.data.ix[0,:],data_2= data_1.data.ix[2,:],col = ['v1','v2']))
 
