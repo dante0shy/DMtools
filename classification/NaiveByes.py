16f465db1668202664a89dbedeab85644a0729a0
dante0shy <dante0shy@yahoo.com>
2016-09-06 13:57:24 +0800

    some changes

---
+++ classification/NaiveByes.py
@@ -0,0 +1,15 @@
+__author__ = 'dante0shy'
+__version__ = '0.1'
+__date__ = '25/08/2016'
+
+
+from data_dis import data_dis
+from data_pro import data_pro
+import numpy as np
+import scipy as sp
+import pandas as pd
+import sys
+import log
+import math
+from operator import itemgetter
+
